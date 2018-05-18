f6922bedac40c9482b0ac323066b21922d923b97
Thomas Shafee <TS404@users.noreply.github.com>
2018-05-18 16:08:52 +1000

    skeleton function to find Nglcs

---
+++ R/pred_nglc.R
@@ -0,0 +1,10 @@
+pred_nglc <- function(sequences){
+  
+  nglc <- gregexpr('N[^P][ST]', sequences, ignore.case = FALSE)
+  countnglc <- unlist(lapply(nglc,length))
+  
+  nglc2 <- data.frame(id          = rep(names(sequences),countnglc),
+                      align_start = unlist(nglc))
+  
+  nglc2
+}
